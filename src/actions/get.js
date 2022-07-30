3b090690ac0d288d2c03cf6cd81f8639171f8e8c
Grégory Paul <me@gpaul.me>
2022-07-30 14:36:18 +0200

    Send last-modified and handle if-modified-since behavior

--- src/actions/get.js
+++ src/actions/get.js
@@ -10,10 +10,23 @@ const get = (req, res) => {
     if (!fs.existsSync(filePath)) {
       throw new NotFoundError()
     } else {
-      fs.readFile(filePath, function (err, content) {
+      fs.stat(filePath, (err, stats) => {
         if (err) throw err
-        res.setHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
-        res.end(content)
+        const mtime = stats?.mtime
+        const lastModified = new Date(mtime).toGMTString()
+        res.setHeader("Last-Modified", lastModified)
+        const ifModifiedSince = (
+          req.headers["if-modified-since"] ?? ""
+        ).toLowerCase()
+        if (lastModified.toLowerCase() === ifModifiedSince) {
+          res.status(304).end()
+        } else {
+          fs.readFile(filePath, function (err, content) {
+            if (err) throw err
+            res.setHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
+            res.end(content)
+          })
+        }
       })
     }
   } catch (err) {
