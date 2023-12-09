3f3f6ef3894013408175bdb4ea7b6e36e55d3d59
Grégory Paul <me@gpaul.me>
2023-12-09 14:12:58 +0100

    add no-cache

--- src/actions/get.js
+++ src/actions/get.js
@@ -1,37 +1,36 @@
-const fs = require("fs")
-const { getFilePath } = require("../file")
-const { NotFoundError, handleError } = require("../errors")
-const { CONTENT_TYPE, CONTENT_TYPE_JSON } = require("../http")
+const fs = require("fs");
+const { getFilePath } = require("../file");
+const { NotFoundError, handleError } = require("../errors");
+const { CONTENT_TYPE, CONTENT_TYPE_JSON, CACHE_CONTROL, NO_CACHE } = require("../http");
 
 const get = (req, res) => {
   try {
-    const filePath = getFilePath(req, res)
+    const filePath = getFilePath(req, res);
 
     if (!fs.existsSync(filePath)) {
-      throw new NotFoundError()
+      throw new NotFoundError();
     } else {
       fs.stat(filePath, (err, stats) => {
-        if (err) throw err
-        const mtime = stats?.mtime
-        const lastModified = new Date(mtime).toGMTString()
-        res.setHeader("Last-Modified", lastModified)
-        const ifModifiedSince = (
-          req.headers["if-modified-since"] ?? ""
-        ).toLowerCase()
+        if (err) throw err;
+        const mtime = stats?.mtime;
+        const lastModified = new Date(mtime).toGMTString();
+        res.setHeader("Last-Modified", lastModified);
+        const ifModifiedSince = (req.headers["if-modified-since"] ?? "").toLowerCase();
         if (lastModified.toLowerCase() === ifModifiedSince) {
-          res.status(304).end()
+          res.status(304).end();
         } else {
           fs.readFile(filePath, function (err, content) {
-            if (err) throw err
-            res.setHeader(CONTENT_TYPE, CONTENT_TYPE_JSON)
-            res.end(content)
-          })
+            if (err) throw err;
+            res.setHeader(CONTENT_TYPE, CONTENT_TYPE_JSON);
+            res.setHeader(CACHE_CONTROL, NO_CACHE);
+            res.end(content);
+          });
         }
-      })
+      });
     }
   } catch (err) {
-    handleError(res, err)
+    handleError(res, err);
   }
-}
+};
 
-module.exports = get
+module.exports = get;
--- src/integration-tests/get.spec.js
+++ src/integration-tests/get.spec.js
@@ -1,8 +1,8 @@
-const request = require("supertest")
-const app = require("../app")
+const request = require("supertest");
+const app = require("../app");
 const {
   authorization: { user, password },
-} = require("../settings.json")
+} = require("../settings.json");
 
 describe("Retrieve data from /test/test.json", () => {
   test("should return 200 with JSON data", () =>
@@ -10,40 +10,33 @@ describe("Retrieve data from /test/test.json", () => {
       .get("/test/test.json")
       .auth(user, password)
       .expect("Content-Type", "application/json; charset=utf-8")
+      .expect("Cache-Control", "no-cache")
       .expect(200)
       .then((response) => {
-        expect(response.body).toEqual({ value: 1 })
-      }))
+        expect(response.body).toEqual({ value: 1 });
+      }));
 
   describe("error cases", () => {
     test("should return 403 if bad password", () =>
-      request(app)
-        .get("/test/test.json")
-        .auth(user, "bad password")
-        .expect(403))
+      request(app).get("/test/test.json").auth(user, "bad password").expect(403));
 
     test("should return 403 if bad user", () =>
-      request(app).get("/test/test.json").auth("baduser", password).expect(403))
+      request(app).get("/test/test.json").auth("baduser", password).expect(403));
 
     test("should respond 403 for non existing directory", () =>
-      request(app).get("/unknown/test.json").auth(user, password).expect(403))
+      request(app).get("/unknown/test.json").auth(user, password).expect(403));
 
     test("should respond 404 for not existing file", () =>
-      request(app)
-        .get("/test/notexisting.json")
-        .auth(user, password)
-        .expect(404))
+      request(app).get("/test/notexisting.json").auth(user, password).expect(404));
 
-    test("should respond 404 for empty key", () =>
-      request(app).get("/test/.json").auth(user, password).expect(404))
+    test("should respond 404 for empty key", () => request(app).get("/test/.json").auth(user, password).expect(404));
 
-    test("should respond 404 for empty app", () =>
-      request(app).get("//a.json").auth(user, password).expect(404))
+    test("should respond 404 for empty app", () => request(app).get("//a.json").auth(user, password).expect(404));
 
     test("should respond 400 if incorrect char in key", () =>
-      request(app).get("/test/idée.json").auth(user, password).expect(400))
+      request(app).get("/test/idée.json").auth(user, password).expect(400));
 
     test("should respond 400 if incorrect char in app", () =>
-      request(app).get("/idée/test.json").auth(user, password).expect(400))
-  })
-})
+      request(app).get("/idée/test.json").auth(user, password).expect(400));
+  });
+});
